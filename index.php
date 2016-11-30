b5244227a7cea74749809f0a24d1b48ae6a6c777
Betina Farias <betina.farias@outlook.com>
2016-11-30 16:34:46 -0200

    Saving user artists from spotify

--- index.php
+++ index.php
@@ -22,6 +22,12 @@ $app->delete('/musicians/:id','deleteMusician');
 
 
 // -----------------------------------
+// Artist
+// -----------------------------------
+$app->post('/artists','addArtist');
+
+
+// -----------------------------------
 // States
 // -----------------------------------
 $app->get('/states','getStates');
@@ -144,6 +150,71 @@ function saveMusician($id) {
 
 	echo json_encode($musician);
 }
+
+
+function addArtist(){
+	$request = \Slim\Slim::getInstance()->request();
+	$artist = json_decode($request->getBody());
+
+	// 1. Verificar se já está cadastrado, se já estiver, trazer do banco
+	// -------------------------------------------------------------------
+	$sql = "SELECT
+				*
+			FROM
+				artist
+			WHERE 
+				uri = :uri" ;
+
+	$conn = getConn();
+	$stmt_artist = $conn->prepare($sql);
+	$stmt_artist->bindParam("uri", $artist->uri);
+	$stmt_artist->execute();
+	$fetchedArtist = $stmt_artist->fetchObject();
+
+	if($fetchedArtist) {
+		$artist->id = $fetchedArtist->id;		
+	}
+	else {
+
+		// 2. Caso não esteja, cadastrar no banco e pegar id criada
+		// ------------------------------------------------------------
+
+		$sql = "INSERT INTO artist (
+					name, 
+					image, 
+					uri ) 
+				values (
+					:name, 
+					:image, 
+					:uri ) ";
+		$conn = getConn();
+		$stmt_insert = $conn->prepare($sql);
+
+		$stmt_insert->bindParam("name", $artist->name);
+		$stmt_insert->bindParam("image", $artist->images[0]->url);
+		$stmt_insert->bindParam("uri", $artist->uri);
+		$stmt_insert->execute();
+		$artist->id = $conn->lastInsertId();
+		echo json_encode($artist->id);
+	}
+
+	// 3. Relacionar artista com usuário na tabela musician_artist
+	// ------------------------------------------------------------
+	$sql = "INSERT INTO musician_artist (
+				id_musician, 
+				id_artist ) 
+			values (
+				:id_musician, 
+				:id_artist ) ";
+
+	$conn = getConn();
+	$stm_relac = $conn->prepare($sql);
+	$stm_relac->bindParam("id_musician", $artist->musicianId);
+	$stm_relac->bindParam("id_artist", $artist->id);
+	$stm_relac->execute();
+
+}
+
 
 function addMusician() {
 	$request = \Slim\Slim::getInstance()->request();
